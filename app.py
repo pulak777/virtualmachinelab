fa407e285a457593e137536acbf8e4a2a319eb8f
pulak777 <pulakdeyashi@gmail.com>
2020-11-23 17:45:55 +0530

    Theory parts are added for experiment 2 and 3,

--- app.py
+++ app.py
@@ -11,9 +11,17 @@ def home():
 def exp9():
     return render_template('simulations/sim9.html')
 
+@app.route('/speed_control_of_3-phase_squirrel_cage_induction_motor_by_voltage_control')
+def theo2():
+    return render_template('theories/theo2.html')
+
 @app.route('/speed_control_of_3-phase_squirrel_cage_induction_motor_by_voltage_control/simulation')
 def exp2():
     return render_template('simulations/sim2.html')
+
+@app.route('/speed_control_of_3-phase_squirrel_cage_induction_motor_by_frequency_control')
+def theo3():
+    return render_template('theories/theo3.html')
 
 @app.route('/speed_control_of_3-phase_squirrel_cage_induction_motor_by_frequency_control/simulation')
 def exp3():
